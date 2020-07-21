bf1ca1aad3b32dbb7923167d89d6833b9bc27ad8
nk <nk@pop-os.localdomain>
2020-07-21 18:43:49 -0500

    base stuff to install i keept forgetting

---
+++ pop_os_setup/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install driver for second GPU
+sudo apt-get install -y system76-driver-nvidia
+
+# install video codecs for twitch streams
+sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+
+# everyone needs vlc eventually
+sudo apt-get install -y vlc
+
