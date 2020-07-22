eaa18a142bdfdc67bf88fee8a17a686adbfa0823
nk <nk@pop-os.localdomain>
2020-07-21 19:02:11 -0500

    tweak

--- pop_os_setup/setup.sh
+++ pop_os_setup/setup.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y system76-driver-nvidia
 
 # install video codecs for twitch streams
 sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+sudo dpkg-reconfigure libdvd-pkg
 
 # everyone needs vlc eventually
 sudo apt-get install -y vlc
