850ccf29226abfe92ae01a417561a37b93f093c0
nk <nk@pop-os.localdomain>
2020-07-22 20:02:26 -0600

    big brain moves

--- pop_os_setup/fix-nvidia-bullshit.sh
+++ pop_os_setup/fix-nvidia-bullshit.sh
@@ -1,17 +1,26 @@
-systemctl stop nvidia-persistenced
-systemctl disable nvidia-persistenced
+#systemctl stop nvidia-persistenced
+#systemctl disable nvidia-persistenced
 
+#unbind nvidia card from X11
+sudo system76-power graphics integrated
+
+#restart to free nvidia card from X11
+sudo systemctl restart gdm
+# if you have running processes this didnt work lmao RIP
+
+# see if it worked?
+# nvidia-smi
+
+
+### old
 #if you have one card this will probably fk u btw
 # https://support.system76.com/articles/graphics-switch-pop/
-sudo system76-power graphics integrated
+# sudo system76-power graphics integrated
 # run this idiot:
 # sudo system76-power graphics nvidia
 
 # goodbye mr gui
-sudo systemctl stop gdm
+#sudo systemctl stop gdm
 
 # plz come back sir
-sudo systemctl start gdm
-
-# if you have running processes this didnt work lmao RIP
-nvidia-smi
+#sudo systemctl start gdm
