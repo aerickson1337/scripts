ee841fbf9bcb2118f97b86531b1ba1dc67935065
nk <nk@pop-os.localdomain>
2020-07-23 21:33:12 -0600

    after 10 pages of purple links on google.com i found the problem

--- pop_os_setup/fixedit.sh
+++ pop_os_setup/fixedit.sh
@@ -1,3 +1,7 @@
 sudo kernelstub --add-options "kvm.ignore_msrs=1 vfio-pci.ids=10de:1e84,10de:10f8,10de:1ad8,10de:1ad9"
 sudo kernelstub -o "amd_iommu=on amd_iommu=pt"
 
+# the info that dun did it
+https://www.windowscentral.com/how-convert-mbr-disk-gpt-move-bios-uefi-windows-10
+https://www.reddit.com/r/VFIO/comments/gkb55c/my_windows_10_guest_os_emulated_with_qemu_kvm_is/
+
