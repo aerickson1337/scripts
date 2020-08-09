0aa7d9cdbcff322e8d6d5b8c66664715d067442d
nk <nk@pop-os.localdomain>
2020-08-08 22:15:07 -0500

    add thing

--- fixboot.sh
+++ fixboot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# udevadm monitor
 sudo systemctl stop systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
 sleep 5
 sudo systemctl start systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket
